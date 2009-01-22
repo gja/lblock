b87ccaa0b305b59fa6d836b9c0ab404f24296c89
Tejas Dinkar <tejas@gja.in>
2009-01-22 23:29:18 +0530

    Engine Complete. Move around a Square

--- constants.h
+++ constants.h
@@ -3,5 +3,19 @@
 
 #define LEFT_RIGHT_STEP 4
 #define UP_DOWN_STEP 4
+#define WALK_STEP 1.
+#define MAX_LOOKUP 60
+
+#define ESCAPE_KEY Qt::Key_Escape
+
+#define LOOK_LEFT_KEY Qt::Key_Left
+#define LOOK_RIGHT_KEY Qt::Key_Right
+#define LOOK_UP_KEY Qt::Key_Up
+#define LOOK_DOWN_KEY Qt::Key_Down
+
+#define MOVE_FORWARD_KEY Qt::Key_W
+#define MOVE_BACKWARD_KEY Qt::Key_S
+#define MOVE_LEFT_KEY Qt::Key_A
+#define MOVE_RIGHT_KEY Qt::Key_D
 
 #endif // CONSTANTS_H
--- glbox.cpp
+++ glbox.cpp
@@ -71,34 +71,56 @@ void GLBox::resizeGL( int w, int h )
 
 void GLBox::keyPressEvent( QKeyEvent *e)
 {
+	double angle = left_right * M_PI / 180.;
+
 	switch( e->key() )
 	{
-		case Qt::Key_Escape:
+		case ESCAPE_KEY:
 			close();
 			break;
 
-		case Qt::Key_Left:
+		case LOOK_LEFT_KEY:
 			left_right -= LEFT_RIGHT_STEP;
 			if (left_right < 0)
 				left_right += 360;
 			break;
 
-		case Qt::Key_Right:
+		case LOOK_RIGHT_KEY:
 			left_right += LEFT_RIGHT_STEP;
 			if (left_right >= 360)
 				left_right -= 360;
 			break;
 
-		case Qt::Key_PageUp:
+		case LOOK_UP_KEY:
 			up_down += UP_DOWN_STEP;
-			if (up_down > 60)
+			if (up_down > MAX_LOOKUP)
 				up_down = 60;
 			break;
 
-		case Qt::Key_PageDown:
+		case LOOK_DOWN_KEY:
 			up_down -= UP_DOWN_STEP;
-			if (up_down < -60)
+			if (up_down < -MAX_LOOKUP)
 				up_down = -60;
+			break;
+
+		case MOVE_FORWARD_KEY:
+			posx += WALK_STEP * sin(angle);
+			posz -= WALK_STEP * cos(angle);
+			break;
+
+		case MOVE_BACKWARD_KEY:
+			posx -= WALK_STEP * sin(angle);
+			posz += WALK_STEP * cos(angle);
+			break;
+
+		case MOVE_LEFT_KEY:
+			posx -= WALK_STEP * cos(angle);
+			posz -= WALK_STEP * sin(angle);
+			break;
+
+		case MOVE_RIGHT_KEY:
+			posx += WALK_STEP * cos(angle);
+			posz += WALK_STEP * sin(angle);
 			break;
 	}
 
