64e0e1ac89ac1da0c285d5464bd93215caae0a7c
Tejas Dinkar <tejas@gja.in>
2009-01-22 22:14:29 +0530

    Added Support for an object in a list

--- glbox.cpp
+++ glbox.cpp
@@ -12,6 +12,7 @@ GLBox::GLBox( QWidget *parent) : QGLWidget(parent)
 GLBox::~GLBox()
 {
 	makeCurrent();
+	glDeleteLists(object, 1);
 }
 
 // Paint the box. The actual openGL commands for drawing the box are performed here.
@@ -19,7 +20,10 @@ void GLBox::paintGL()
 {
 	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 	glLoadIdentity();
+
+	glCallList(object);
 }
+
 
 // Set up the OpenGL rendering state, and define display list
 void GLBox::initializeGL()
@@ -32,16 +36,21 @@ void GLBox::initializeGL()
 	glDepthFunc(GL_LEQUAL);
 
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	object = glGenLists(1);
+	void drawObject();
+	glNewList(object, GL_COMPILE);
+		drawObject();
+	glEndList();
 }
 
 // Set up the OpenGL view port, matrix mode, etc.
 void GLBox::resizeGL( int w, int h )
 {
-
 	glViewport( 0, 0, (GLint)w, (GLint)h );
 	glMatrixMode( GL_PROJECTION );
 	glLoadIdentity();
-	glFrustum( -1.0, 1.0, -1.0, 1.0, 5.0, 20.0 );
+	gluPerspective(45.0, (double) w / (double) h, 1.0, 100.0);
 	glMatrixMode( GL_MODELVIEW );
 	glLoadIdentity();
 }
--- glbox.h
+++ glbox.h
@@ -17,7 +17,12 @@ class GLBox : public QGLWidget
 	void initializeGL();
 	void paintGL();
 	void resizeGL( int w, int h );
+
+	// Called Whenever a Key is Pressed
 	virtual void keyPressEvent( QKeyEvent *event );
+
+	// This is a list to hold the object we are viewing
+	GLuint object;
 
     public slots:
 };
