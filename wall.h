f11328fad52d07fbc4b804dd5327191bb0f4a8fe
Tejas Dinkar <tejas@gja.in>
2009-01-24 18:54:05 +0530

    A bit of documentation and fixed a Compile Bug. Thanks Thejaswi

--- wall.h
+++ wall.h
@@ -1,24 +1,23 @@
 #ifndef WALL_H
 #define WALL_H
-#include "item.h"
 
+#include "item.h"
 
 class Wall : public Item
 {
-  
     protected:
-
-  	GLuint object;
- 	GLfloat height;
- 	GLfloat thickness;
+	GLfloat height;
+	GLfloat thickness;
 
     public:
 
+	// These two functions manipulate the height of the wall
 	inline void setHeight(float x)  { height=x; }
- 	inline float getHeight()  { return height; }
+	inline float getHeight()  { return height; }
+
+	// These two functions helps manipulate the thickness of the wall
 	inline void setThickness(float x)  { thickness=x;  }
 	inline float getThickness()  { return thickness; }
-		
 };
 
 #endif
