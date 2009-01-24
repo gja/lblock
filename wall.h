9821e2e9f795dcb57d52d0f3f87543279b8734d6
thejaswi <thejask@gmail.com>
2009-01-24 13:14:15 +0000

    check if the indentation is correct

--- wall.h
+++ wall.h
@@ -3,18 +3,21 @@
 #include "item.h"
 
 
-class wall : public item
+class Wall : public Item
 {
   
-  	protected:
-  		GLuint object;
- 		GLfloat height;
- 		GLfloat thickness;
-	public:
-		inline void setHeight(float x)  { height=x; }
- 		inline void getHeight()  { return height; }
-		inline void setThickness(float x)  { thickness=x;  }
-		inline void getThickness()  { return thickness; }
+    protected:
+
+  	GLuint object;
+ 	GLfloat height;
+ 	GLfloat thickness;
+
+    public:
+
+	inline void setHeight(float x)  { height=x; }
+ 	inline float getHeight()  { return height; }
+	inline void setThickness(float x)  { thickness=x;  }
+	inline float getThickness()  { return thickness; }
 		
 };
 
