1418124e5c806184e377a8b954a7bfaade200934
Tejas Dinkar <tejas@gja.in>
2009-01-24 18:54:05 +0530

    A bit of documentation and fixed a Compile Bug. Thanks Thejaswi

--- item.h
+++ item.h
@@ -3,21 +3,18 @@
 
 class Item 
 {
-
     protected:
-
 	GLuint object;
-  	GLfloat posx, posy, poz;
+	GLfloat posx, posy, posz;
 
     public:
-
+	// This function is used to set the position of the object
 	inline void setPos(float x,float y,float z) { posx=x; posy=y; posz=z; }
+
+	// These next three functions return the position
 	inline float getPosX() { return posx; }
 	inline float getPosY() { return posy; }
 	inline float getPosZ() { return posz; }
-	
-
-      		
 };
 
 #endif 
