7d8a4537e82637b07b96beb9c191dad9cd2ee130
Tejas Dinkar <tejas@gja.in>
2009-01-26 12:47:25 +0530

    added the text values of the textures

--- drawobject.cpp
+++ drawobject.cpp
@@ -10,9 +10,15 @@
 
 void GLBox::loadAllTextures()
 {
-	textures["sand"] = Texture(0xFFE303, "data/sand.png", 8.0f, 8.0f);
-	textures["maroon"] = Texture(0x822222);
+	textures["sand"] = Texture(0xCBAB88, "data/sand.png", 8.0f, 8.0f);
+	textures["maroon"] = Texture(0xB87353, "data/maroon.png");
 	textures["glass"] = Texture(0xF000FFFF, "data/glass.png");
+	textures["window"] = Texture(0xF000FFFF, "data/window.png");
+	textures["brown"] = Texture(0xBFAA82, "data/brown.png");
+	textures["creamwall"] = Texture(0xA2865D, "data/creamwall.png");
+	textures["lightwood"] = Texture(0xAB8560, "data/lightwood.png");
+	textures["darkwood"] = Texture(0x653626, "data/darkwood.png");
+	textures["marblefloor"] = Texture(0x8B8C8D, "data/marblefloor.png");
 }
 
 void GLBox::drawObject()
