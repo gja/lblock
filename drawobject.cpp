c9079c8110df6969fb43c5d562b939f822325e53
Tejas Dinkar <tejas@gja.in>
2009-01-30 18:10:43 +0530

    The Left Wing is Complete

--- drawobject.cpp
+++ drawobject.cpp
@@ -29,6 +29,8 @@ void GLBox::drawObject()
 	
 	// First, the left side of the building
 	for (int i = 0; i < 3; i++) {
+
+		// Closest Wall
 		wall = new Wall(-50.0f, i * 10.0f, -50.0f, 0.0f, 25.0f, textures["creamwall"], textures["maroon"]);
 		wall->addWindow(1.0f, 4.0f, textures["window"], 4.0f, 7.0f);
 		wall->addWindow(7.0f, 4.0f, textures["window"], 4.0f, 7.0f);
@@ -36,6 +38,7 @@ void GLBox::drawObject()
 		wall->addWindow(19.0f, 4.0f, textures["window"], 4.0f, 7.0f);
 		addObject(wall);
 
+		// Visible tall wall, inside of l
 		wall = new Wall(-25.0f, i * 10.0f, -50.0f, 90.0f, 37.0f, textures["creamwall"], textures["maroon"]);
 		wall->addWindow(1.0f, 4.0f, textures["window"], 4.0f, 7.0f);
 		wall->addWindow(7.0f, 4.0f, textures["window"], 4.0f, 7.0f);
@@ -51,15 +54,42 @@ void GLBox::drawObject()
 		wall = new Wall(-50.0f, i * 10.0f, -87.0f, 270.0f, 37.0f, textures["creamwall"], textures["maroon"]);
 		addObject(wall);
 
+		// Wall with doors
 		wall = new Wall(-31.0f, i * 10.0f, -131.0f, 270.0f, 44.0f, textures["creamwall"], textures["maroon"]);
+		// First Class Room
+		wall->addWindow(6.0f, 5.0f, textures["darkwood"], 0.0f);
+		wall->addWindow(13.0f, 4.0f, textures["window"], 0.0f);
+		wall->addWindow(19.0f, 4.0f, textures["window"], 0.0f);
+		// Second Class Room
+		wall->addWindow(26.0f, 5.0f, textures["darkwood"], 0.0f);
+		wall->addWindow(33.0f, 4.0f, textures["window"], 0.0f);
+		wall->addWindow(39.0f, 4.0f, textures["window"], 0.0f);
 		addObject(wall);
 
-		wall = new Wall(-25.0f, i * 10.0f, -87.0f, 90.0f, 44.0f, textures["creamwall"], textures["maroon"], 4.0f);
+		// Railing Wall
+		wall = new Wall(-25.0f, i * 10.0f, -87.0f, 90.0f, 44.0f, textures["creamwall"], i == 0 ? textures["maroon"] : textures["brown"], 4.0f);
 		addObject(wall);
 
-		wall = new Wall(-25.0f, i* 10.0f + 4.25f, -87.0f, 90.0f, 44.0f, textures["black"], textures["black"], .25f);
+		// Pillars
+		wall = new Wall(-25.0f, i * 10.0f + 4.0f, -100.0f, 90.0f, 2.0f, textures["creamwall"], textures["brown"], 6.0f);
+		addObject(wall);
+		wall = new Wall(-25.0f, i * 10.0f + 4.0f, -114.0f, 90.0f, 2.0f, textures["creamwall"], textures["brown"], 6.0f);
+		addObject(wall);
+
+		// Railing
+		wall = new Wall(-25.0f, i* 10.0f + 4.25f, -87.0f, 90.0f, 13.0f, textures["black"], textures["black"], .25f);
+		addObject(wall);
+		wall = new Wall(-25.0f, i* 10.0f + 4.25f, -102.0f, 90.0f, 12.0f, textures["black"], textures["black"], .25f);
+		addObject(wall);
+		wall = new Wall(-25.0f, i* 10.0f + 4.25f, -116.0f, 90.0f, 15.0f, textures["black"], textures["black"], .25f);
+		addObject(wall);
+
+		// Main Door (at end of the hall)
+		wall = new Wall(-31.0f, i * 10.0f, -87.0f, 0.0f, 5.5f, textures["creamwall"], textures["creamwall"]);
+		wall->addWindow(1.0f, 4.0f, textures["darkwood"], 0.0f);
 		addObject(wall);
 		
+		// Now the floors
 		floor = new Floor(-50.0f, i * 10.0f, -50.0f, 0.0f, textures["creamwall"], textures["marblefloor"]);
 		floor->addPoint(25.0f, 0.0f);
 		floor->addPoint(25.0f, 37.0f);
