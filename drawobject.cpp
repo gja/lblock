c7132588e7122e66909ef4224a8ba9ce9284bdb3
Tejas Dinkar <tejas@gja.in>
2009-02-07 21:17:01 +0530

    Added Windows and Doors to the XML file

--- drawobject.cpp
+++ drawobject.cpp
@@ -51,6 +51,29 @@ void GLBox::drawObject(const QDomDocument &doc)
 			qDebug()<<"Adding Wall:"<<x<<y<<z<<rotation<<length<<innerTexture<<outerTexture<<height<<thickness;
 
 			Wall *wall = new Wall(x.toFloat(), y.toFloat(), z.toFloat(), rotation.toFloat(), length.toFloat(), textures[innerTexture], textures[outerTexture], height.toFloat(), thickness.toFloat());
+
+			// now we start parsing the windows
+			for (QDomNode w = e.firstChild(); !w.isNull(); w = w.nextSibling()) {
+				QDomElement tmp = w.toElement();
+
+				if (tmp.tagName() == "window") {
+					QString position = tmp.attribute("position");
+					QString length = tmp.attribute("length");
+					QString texture = tmp.attribute("texture");
+					QString lowerHeight = tmp.attribute("lowerHeight", "3.0");
+					QString upperHeight = tmp.attribute("upperHeight", "7.0");
+					wall->addWindow(position.toFloat(), length.toFloat(), textures[texture], lowerHeight.toFloat(), upperHeight.toFloat());
+					qDebug()<<position<<length<<texture<<lowerHeight<<upperHeight;
+				}
+				else if (tmp.tagName() == "door") {
+					QString position = tmp.attribute("position");
+					QString length = tmp.attribute("length");
+					QString texture = tmp.attribute("texture");
+					QString height = tmp.attribute("height", "7.0");
+					wall->addDoor(position.toFloat(), length.toFloat(), textures[texture], height.toFloat());
+				}
+			}
+
 			addObject(wall);
 		}
 	}
