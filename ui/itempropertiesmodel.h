e2ae4a74f0837d6c0c9ed0930636dc6e55e49f49
unknown <sourav sarkar@.(none)>
2009-05-18 13:04:25 +0530

    finished documenting itempropertiesmodel

--- ui/itempropertiesmodel.h
+++ ui/itempropertiesmodel.h
@@ -5,15 +5,18 @@
 
 #include <QStandardItemModel>
 #include <QList>
-
+/**
+ * This class conatins the properties of the item in the model format.
+ */
 class ItemPropertiesModel : public QStandardItemModel {
 	LBlockValues elem;
-
+	/// This is th list of all the items
 	QList<QStandardItem *> items;
 
     public:
+    	/// This is the constuctor with paprameters
 	ItemPropertiesModel(const LBlockValues &e);
-
+	/// This is the empty constructor
 	ItemPropertiesModel();
 
 	~ItemPropertiesModel();
