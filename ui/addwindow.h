3cf573c56ade4d9788f32406abfd33a6d306f7ad
unknown <sourav sarkar@.(none)>
2009-05-18 11:53:10 +0530

    Finished adding comments to addwindow

--- ui/addwindow.h
+++ ui/addwindow.h
@@ -3,12 +3,19 @@
 
 #include <QDialog>
 
+/**
+ * This class shows the dialog to take in the attributes of the window.
+ * It pops up a dialog for taking in the attributes of the window like the 
+ * length, height and position
+ */
 namespace Ui {
 	class AddWindow;
 }
 
 class AddWindow : public QDialog {
 	Q_OBJECT
+	
+	/// This holds all the UI related objects.
 
 	Ui::AddWindow *ui;
 
